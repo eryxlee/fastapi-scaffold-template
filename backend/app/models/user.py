559f201082fb8ebad898e164afd79c91baba7116
Eryx Lee <eryx.lee0901@gmail.com>
2024-04-23 22:48:49 +0800

    feat: add user models

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -32,3 +32,54 @@ class User(Base, CommonMixin):
     # def verify_password(self, raw_password):
     #     # 使用passlib验证密码
     #     return verify_password(raw_password, self.hashed_password)
+
+
+class Role(Base, CommonMixin):
+    """ 角色表 """
+
+    id: Mapped[idPk]
+    name: Mapped[str] = mapped_column(String(32), comment="角色名称")
+    code: Mapped[str] = mapped_column(String(32), comment="角色code")
+    description: Mapped[str | None] = mapped_column(String(60), comment="角色描述")
+
+    user_role = relationship("UserRole", back_populates="role")
+    role_resource = relationship("RoleResource", back_populates="role")
+
+
+class Resource(Base, CommonMixin):
+    """ 资源表 """
+
+    id: Mapped[idPk]
+    name: Mapped[str] = mapped_column(String(32), comment="资源名称")
+    level: Mapped[int] = mapped_column(SmallInteger, server_default=text('0'), comment="层级: 0 目录 1 菜单 2 权限")
+    pid: Mapped[int] = mapped_column(server_default=text('0'), comment="父节点id")
+    icon: Mapped[str | None] = mapped_column(String(64), comment="图标")
+    menu_url: Mapped[str | None] = mapped_column(String(64), comment="页面路由")
+    request_url: Mapped[str | None] = mapped_column(String(64), comment="请求url")
+    permission_code: Mapped[str | None] = mapped_column(String(32), comment="权限code")
+
+    role_resource = relationship("RoleResource", back_populates="resource")
+
+
+class UserRole(Base):
+    """ 用户角色表 """
+
+    id: Mapped[idPk]
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), comment="用户id")
+    role_id: Mapped[int] = mapped_column(ForeignKey("role.id"), comment="角色id")
+
+    user = relationship("User", back_populates="user_role")
+    role = relationship("Role", back_populates="user_role")
+
+
+class RoleResource(Base):
+    """ 角色资源表 """
+
+    id: Mapped[idPk]
+    role_id: Mapped[int] = mapped_column(ForeignKey("role.id"), comment="角色id")
+    resource_id: Mapped[int] = mapped_column(ForeignKey("resource.id"), comment="资源id")
+
+    role = relationship("Role", back_populates="role_resource")
+    resource = relationship("Resource", back_populates="role_resource")
+
+
