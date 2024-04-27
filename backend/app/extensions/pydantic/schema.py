b8ff687470e56259361d7d5bd5eae02b0a4f4ef2
Eryx Lee <eryx.lee0901@gmail.com>
2024-04-27 23:54:49 +0800

    feat: add base schema

---
+++ backend/app/extensions/pydantic/schema.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+
+from pydantic import BaseModel, validator
+
+
+class CommonSchemaMixin(BaseModel):
+    id: int
+    create_time: datetime | None
+    update_time: datetime | None
+
+    class Config:
+        # from_attributes 将告诉 Pydantic 模型读取数据，即它不是一个 dict，而是一个 ORM 模型
+        # https://docs.pydantic.dev/2.0/usage/models/#arbitrary-class-instances
+        from_attributes=True
+        # 自定义编码器
+        json_encoders={datetime: lambda dt: dt.strftime("%Y-%m-%d %H:%M:%S")}
