f209484d72424ee4dbb696d61511c889821cf16d
Eryx Lee <eryx.lee0901@gmail.com>
2024-05-13 20:43:33 +0800

    feat: add pagination extension

--- backend/app/extensions/fastapi/pagination.py
+++ backend/app/extensions/fastapi/pagination.py
@@ -3,19 +3,35 @@
 from typing import Annotated
 
 from fastapi import Depends
-from pydantic import BaseModel
+from pydantic import BaseModel, computed_field
+from sqlmodel import Field, SQLModel
 
 
-class PageSchemaIn(BaseModel):
+class PageSchemaIn(SQLModel):
     """ 分页查询参数 """
     page: int = 1
     page_size: int = 10
 
+    @computed_field
+    @property
+    def offset(self) -> int:
+        return (self.page - 1) * self.page_size
 
-class PageSchemaOut(BaseModel):
+    @computed_field
+    @property
+    def limit(self) -> int:
+        return self.page_size
+
+class PageSchemaOut(SQLModel):
     """ 分页查询参数 """
     page: int = 1
     page_size: int = 10
+    total: int = 0
+
+    @computed_field
+    @property
+    def pages(self) -> int:
+        return (self.total - 1) // self.page_size + 1
 
 def page_query(page: int = 1, page_size: int = 10):
     """ 获取查询参数 """
