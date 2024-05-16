5c4c71c83698d68e3ad50260377f1da8e63cb14e
Eryx Lee <eryx.lee0901@gmail.com>
2024-05-16 09:27:36 +0800

    chore: update get_session by remove sessionmake

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -23,7 +23,7 @@ from sqlmodel.ext.asyncio.session import AsyncSession as Session
 from app.config import settings
 
 # 采用配置变量创建数据库引擎
-engine = create_async_engine(
+async_engine = create_async_engine(
     settings.AIO_MARIADB_DATABASE_URI.unicode_string(),
     echo=settings.DB_ENABLE_ECHO,
     pool_size=settings.DB_POOL_SIZE,
@@ -31,10 +31,11 @@ engine = create_async_engine(
     future=True
 )
 
+async_session = sessionmaker(
+    async_engine, class_=Session, expire_on_commit=False
+)
+
 async def get_session() -> AsyncGenerator[Session, None, None]:
-    async_session = sessionmaker(
-        engine, class_=Session, expire_on_commit=False
-    )
     async with async_session() as session:
         yield session
 
@@ -42,7 +43,7 @@ async def get_session() -> AsyncGenerator[Session, None, None]:
 # metadata.create_all doesn't execute asynchronously, so we used run_sync
 # to execute it synchronously within the async function.
 async def init_db() -> None:
-    async with engine.begin() as conn:
+    async with async_engine.begin() as conn:
         # await conn.run_sync(SQLModel.metadata.drop_all)
         await conn.run_sync(SQLModel.metadata.create_all)
 
