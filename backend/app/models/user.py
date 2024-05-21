8f66c9fb9a0418e5c6b52f5641ef5cc282776866
Eryx Lee <eryx.lee0901@gmail.com>
2024-05-21 18:59:21 +0800

    feat: add role resource models

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -6,11 +6,12 @@ from passlib.hash import pbkdf2_sha256
 
 from sqlalchemy import (
     String,
+    Integer,
     SmallInteger,
 )
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
 
-from . import TimestampModel, IDModel, TableBase, CommonPropertyModel
+from . import TimestampModel, IDModel, Metadata, TableBase, CommonPropertyModel
 
 
 class UserBase(SQLModel):
@@ -20,6 +21,7 @@ class UserBase(SQLModel):
     email: EmailStr = Field(default=None, max_length=128, nullable=True, description="邮件", sa_type=String(128))
     gender: int = Field(default=None, nullable=True, description="性别", sa_type=SmallInteger)
     phone: str = Field(default=None, max_length=36, nullable=True, description="电话", sa_type=String(36))
+    role_id: int= Field(default=None, foreign_key="role.id")
 
 
 class UserCreate(UserBase):
@@ -37,10 +39,16 @@ class User(
     CommonPropertyModel,
     UserCreate,
     IDModel,
+    Metadata,
     TableBase,
     table = True):
     """ 展示用户详情信息的用户模型 """
     is_active: int = Field(default=0, nullable=True, description="是否激活", sa_type=SmallInteger)
+
+    # 多对一关系，需要定义一个role_id字段
+    role: Optional["Role"] = Relationship(
+        back_populates="users", sa_relationship_kwargs={"lazy": "joined"}
+    )
 
     def verify_password(self, raw_password) -> bool:
         # 使用passlib验证密码
@@ -51,80 +59,55 @@ class User(
         return pbkdf2_sha256.hash(raw_password)
 
 
+class RoleResourceLink(Metadata, table=True):
+    role_id: int | None = Field(default=None, foreign_key="role.id", primary_key=True)
+    resource_id: int | None = Field(default=None, foreign_key="resource.id", primary_key=True)
 
 
+class RoleBase(SQLModel):
+    name: str = Field(max_length=32, description="角色名称", sa_type=String(32))
+    code: str = Field(max_length=32, description="角色code", sa_type=String(32))
+    description: str = Field(default=None, max_length=255, nullable=True, description="描述", sa_type=String(255))
 
 
+class Role(
+    TimestampModel,
+    CommonPropertyModel,
+    RoleBase,
+    IDModel,
+    Metadata,
+    TableBase,
+    table=True):
+    users: list[User] = Relationship(
+        back_populates="role", sa_relationship_kwargs={"lazy": "selectin"}
+    )
+    resources: list["Resource"] = Relationship(
+        back_populates="roles",
+        link_model=RoleResourceLink,
+        sa_relationship_kwargs={"lazy": "selectin"}
+    )
 
 
-# class User(Base, CommonMixin):
-
-#     id: Mapped[idPk]
-#     name: Mapped[str] = mapped_column(String(60), unique=True, comment="用户名")
-#     password: Mapped[str] = mapped_column(String(128), comment="密码")
-#     avatar: Mapped[str | None] = mapped_column(String(127), comment="头像")
-#     email: Mapped[str | None] = mapped_column(String(60), comment="邮件地址")
-#     gender: Mapped[int | None] = mapped_column(SmallInteger, server_default=text('0'), comment="性别: 0 未知 1 男 2 女")
-#     phone: Mapped[str | None] = mapped_column(String(30), comment="手机号")
-#     is_active: Mapped[int | None] = mapped_column(SmallInteger, server_default=text('0'), comment="可用：0 可用，1 禁用")
-
-#     user_role = relationship("UserRole", back_populates="user")
-
-#     # 用户与TODO的关联关系可以在这里定义
-#     # todos = relationship("Todo", back_populates="owner")
-
-#     def verify_password(self, raw_password) -> bool:
-#         # 使用passlib验证密码
-#         return pbkdf2_sha256.verify(raw_password, self.password)
-
-#     def set_encrypt_password(self, raw_password) -> None:
-#         self.password = pbkdf2_sha256.hash(raw_password)
+class ResourceBase(SQLModel):
+    name: str = Field(max_length=32, description="资源名称", sa_type=String(32))
+    level: int = Field(default=0, description="层级: 0 目录 1 菜单 2 权限", sa_type=SmallInteger)
+    pid: int = Field(default=0, description="父节点id", sa_type=Integer)
+    icon: str = Field(default=None, max_length=64, nullable=True, description="图标", sa_type=String(64))
+    menu_url: str = Field(default=None, max_length=64, nullable=True, description="页面路由", sa_type=String(64))
+    request_url: str = Field(default=None, max_length=64, nullable=True, description="请求url", sa_type=String(64))
+    permission_code: str = Field(default=None, max_length=32, nullable=True, description="权限code", sa_type=String(32))
 
 
-# class Role(Base, CommonMixin):
-#     """ 角色表 """
-
-#     id: Mapped[idPk]
-#     name: Mapped[str] = mapped_column(String(32), comment="角色名称")
-#     code: Mapped[str] = mapped_column(String(32), comment="角色code")
-#     description: Mapped[str | None] = mapped_column(String(60), comment="角色描述")
-
-#     user_role = relationship("UserRole", back_populates="role")
-#     role_resource = relationship("RoleResource", back_populates="role")
-
-
-# class UserRole(Base):
-#     """ 用户角色表 """
-
-#     id: Mapped[idPk]
-#     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), comment="用户id")
-#     role_id: Mapped[int] = mapped_column(ForeignKey("role.id"), comment="角色id")
-
-#     user = relationship("User", back_populates="user_role")
-#     role = relationship("Role", back_populates="user_role")
-
-
-# class Resource(Base, CommonMixin):
-#     """ 资源表 """
-
-#     id: Mapped[idPk]
-#     name: Mapped[str] = mapped_column(String(32), comment="资源名称")
-#     level: Mapped[int] = mapped_column(SmallInteger, server_default=text('0'), comment="层级: 0 目录 1 菜单 2 权限")
-#     pid: Mapped[int] = mapped_column(server_default=text('0'), comment="父节点id")
-#     icon: Mapped[str | None] = mapped_column(String(64), comment="图标")
-#     menu_url: Mapped[str | None] = mapped_column(String(64), comment="页面路由")
-#     request_url: Mapped[str | None] = mapped_column(String(64), comment="请求url")
-#     permission_code: Mapped[str | None] = mapped_column(String(32), comment="权限code")
-
-#     role_resource = relationship("RoleResource", back_populates="resource")
-
-
-# class RoleResource(Base):
-#     """ 角色资源表 """
-
-#     id: Mapped[idPk]
-#     role_id: Mapped[int] = mapped_column(ForeignKey("role.id"), comment="角色id")
-#     resource_id: Mapped[int] = mapped_column(ForeignKey("resource.id"), comment="资源id")
-
-#     role = relationship("Role", back_populates="role_resource")
-#     resource = relationship("Resource", back_populates="role_resource")
+class Resource(
+    TimestampModel,
+    CommonPropertyModel,
+    ResourceBase,
+    IDModel,
+    Metadata,
+    TableBase,
+    table=True):
+    roles: list[Role] = Relationship(
+        back_populates="resources",
+        link_model=RoleResourceLink,
+        sa_relationship_kwargs={"lazy": "selectin"}
+    )
